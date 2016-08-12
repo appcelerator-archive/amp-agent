8b9cf2a60fffc12754b0fd0ef3b9a9404bd2f095
freignat <freignat@axway.com>
2016-08-12 16:34:16 +0200

    update go test appcelerator/amp#15

--- testapp/agent_test.go
+++ testapp/agent_test.go
@@ -5,7 +5,7 @@ import (
 	. "github.com/haoxins/supertest"
 )
 
-//TestAgent test amp-agent using its api health, status 200 means agent has is reading dockers events stream
+//TestAgent test amp-agent using its api health, status 200 means agent is reading its dockers events stream
 func TestAgent(t *testing.T) {
   Request("http://localhost:5001").
     Get("/api/v1/health").
